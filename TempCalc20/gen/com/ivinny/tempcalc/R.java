5d64f9732ae5e31e807ef4aa1b737a9e6eca72e3
Mikel Gonzalez <agcompguru@gmail.com>
2013-05-02 08:25:30 -0400

    week 4

--- TempCalc20/gen/com/ivinny/tempcalc/R.java
+++ TempCalc20/gen/com/ivinny/tempcalc/R.java
@@ -17,31 +17,42 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int weather=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090002;
+        public static final int button2=0x7f090004;
+        public static final int editText1=0x7f090001;
+        public static final int imageView1=0x7f090006;
+        public static final int textView1=0x7f090003;
+        public static final int textView2=0x7f090000;
+        public static final int textView3=0x7f090005;
     }
     public static final class integer {
-        public static final int celsius=0x7f040001;
-        public static final int farenheit=0x7f040000;
-        public static final int kelvin=0x7f040002;
+        public static final int celsius=0x7f050001;
+        public static final int farenheit=0x7f050000;
+        public static final int kelvin=0x7f050002;
+    }
+    public static final class layout {
+        public static final int form=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int c=0x7f050005;
-        public static final int f=0x7f050004;
-        public static final int hello_world=0x7f050002;
-        public static final int k=0x7f050006;
-        public static final int moneyamts=0x7f050003;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int c=0x7f070005;
+        public static final int f=0x7f070004;
+        public static final int hello_world=0x7f070002;
+        public static final int k=0x7f070006;
+        public static final int moneyamts=0x7f070003;
     }
     public static final class style {
         /** 
@@ -65,10 +76,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f060003;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060004;
+        public static final int Theme_MyTheme_Code=0x7f060002;
+        public static final int myStyle=0x7f060000;
+        public static final int myTheme=0x7f060001;
     }
 }
